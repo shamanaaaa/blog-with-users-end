3284b947b1c8966cb10a5f39d764966cdbcc4d43
shamanaaaa <shamanaaaa@gmail.com>
2021-06-07 15:36:19 +0200

    Initial commit.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
-##CONNECT TO DB
+# CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
